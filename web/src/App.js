d6170d1757e09b6f8cf5ba31141201fbf0d3d022
waratpala <97785199+waratpala@users.noreply.github.com>
2023-04-27 14:27:34 +0700

    fix front

--- web/src/App.js
+++ web/src/App.js
@@ -11,20 +11,23 @@ import Noveldetail from './components/NovelDetail';
 import Editnovel from './components/Edit';
 import ReadNovel from './components/Read';
 import Homepage from './components/Home';
-import Login from './components/Login';
+import Page403 from './components/Page403';
+import Page500 from './components/Page500';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/Register" element={<Registpage/>}/>
-            <Route path="/Login" element={<Login/>}/>
-            <Route path="/createnovel" element={<ManageNovel/>}/>
-            <Route path="/naveldetail" element={<Noveldetail/>}/>
-            <Route path="/novelcontent" element={<Novelcontent/>}/>
-            <Route path="/naveledit" element={<Editnovel/>}/>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Register" element={<Registpage />} />
+          <Route path="/createnovel" element={<ManageNovel />} />
+          <Route path="/noveldetail/:fictionid" element={<Noveldetail />} />
+          <Route path="/novelcontent/:fictionid" element={<Novelcontent />} />
+          <Route path="/noveledit/:fictionid/:chapterid" element={<Editnovel />} />
+          <Route path="/novelread/:fictionid/:chapterid" element={<ReadNovel />} />
+          <Route path="/403" element={<Page403 />} />
+          <Route path="/500" element={<Page500 />} />
         </Routes>
       </BrowserRouter>
     </>
